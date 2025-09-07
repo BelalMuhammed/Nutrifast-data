a03077e92c38fba7c37e9698b197e5221c4fe3b0
BelalMuhammed <Belal.Muhammad950@gmail.com>
2025-09-07 20:55:21 +0300

    upload to vercel

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-// server.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Your JSON file
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
-const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+module.exports = server;
